72efb62c5f9025129268f7f31b821191008d698b
Shreya3624 <creativeshreya24@gmail.com>
2024-08-09 12:48:28 +0530

    fragment commit

--- app/src/main/java/com/example/eat_easy/home_page.kt
+++ app/src/main/java/com/example/eat_easy/home_page.kt
@@ -2,10 +2,34 @@ package com.example.eat_easy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationItemView
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class home_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_page)
+        val homefrg=home()
+        val groceryListfrg=grocery_list()
+        val mealPlanningfrg=meal_planning()
+        val profilefrg=profile()
+        makeCurrentFragment(home())
+        val bview:(BottomNavigationView)=findViewById(R.id.bview)
+        bview.setOnNavigationItemSelectedListener{
+            when(it.itemId){
+                R.id.nav_home -> makeCurrentFragment(homefrg)
+                R.id.nav_glist -> makeCurrentFragment(groceryListfrg)
+                R.id.nav_mlist -> makeCurrentFragment(mealPlanningfrg)
+                R.id.nav_profile -> makeCurrentFragment(profilefrg)
+            }
+            true
+        }
     }
+    private fun makeCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fview,fragment)
+            commit()
+        }
 }
