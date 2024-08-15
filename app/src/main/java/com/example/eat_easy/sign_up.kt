841e72b59f33f83e50e76fd239ce7c3658a47231
Shreya3624 <creativeshreya24@gmail.com>
2024-08-16 01:38:20 +0530

    bmi commit

--- app/src/main/java/com/example/eat_easy/sign_up.kt
+++ app/src/main/java/com/example/eat_easy/sign_up.kt
@@ -1,13 +1,12 @@
 package com.example.eat_easy
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class sign_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,41 +17,64 @@ class sign_up : AppCompatActivity() {
         val txt_pass:EditText=findViewById(R.id.txt_pass)
         val txt_weight:EditText=findViewById(R.id.txt_weight)
         val txt_height:EditText=findViewById(R.id.txt_height)
-        val txt_sign:TextView = findViewById(R.id.txt_sign)
         val txt1:TextView = findViewById(R.id.txt1)
+        val btn:Button=findViewById(R.id.btn)
+        var weight:Double
+        var height:Double
+        val emailInput = txt_mail.text.toString()
+
         txt1.setOnClickListener {
             val intent1 = Intent(this,login::class.java)
             startActivity(intent1)
         }
-        val btn:Button=findViewById(R.id.btn)
+
         btn.setOnClickListener {
             if (txt_uname.text.toString().isEmpty()) {
                 txt_uname.setError("Field can't be empty!")
             }
-            fun isValidEmail(email: String): Boolean {
-                val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$".toRegex()
-                return email.matches(emailRegex)
-            }
-            val emailInput = txt_mail.text.toString()
 
-            if (isValidEmail(emailInput)) {
-                val intent = Intent(this,home_page::class.java)
-                startActivity(intent)
-            } else {
-                txt_mail.setError("Please enter valid Email")
-            }
-            if(txt_pass.length()<6){
+
+
+//              if (!isValidEmail(emailInput)) {
+//              //  txt_mail.setError("Please enter valid Email")
+//            }
+
+           else if(txt_pass.length()<6){
                 txt_pass.setError("Maximum 6 character required")
             }
-            if (txt_weight.text.toString().isEmpty()) {
+          else  if (txt_weight.text.toString().isEmpty()) {
                 txt_weight.setError("Field can't be empty!")
+              //  val weight = .toDoubleOrNull()
             }
-            if (txt_height.text.toString().isEmpty()) {
+           else if (txt_height.text.toString().isEmpty()) {
                 txt_height.setError("Field can't be empty!")
             }
+
+            else{
+               weight=(txt_weight.text.toString()).toDouble()
+                height=(txt_height.text.toString()).toDouble()
+                val bmi = calculateBMI(weight, height)
+
+
+                when {
+                    bmi < 18.5 -> Toast.makeText(this,"$bmi underweight",Toast.LENGTH_LONG).show()
+                    bmi in 18.5..24.9 -> Toast.makeText(this,"$bmi normal",Toast.LENGTH_LONG).show()
+                    bmi in 25.0..29.9 -> Toast.makeText(this,"$bmi overweight",Toast.LENGTH_LONG).show()
+                    else ->  Toast.makeText(this,"invalid",Toast.LENGTH_LONG).show()
+                }
+
+            }
+
         }
 
 
+    }
+    fun isValidEmail(email: String): Boolean {
+        val emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$".toRegex()
+        return email.matches(emailRegex)
+    }
+    fun calculateBMI(weight:Double,height:Double):Double {
+        return weight / (height * height)
     }
 }
 
