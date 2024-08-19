7750f43c347771bc95a35737b668880e3a28d9c1
Shreya <creativeshreya24@gmail.com>
2024-08-19 12:01:15 +0530

    meal_design commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,7 @@ dependencies {
     implementation("androidx.annotation:annotation:1.8.2")
     implementation("androidx.gridlayout:gridlayout:1.0.0")
     implementation("androidx.activity:activity-ktx:1.9.1")
+    implementation("androidx.activity:activity:1.9.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
--- app/src/main/java/com/example/eat_easy/meal_planning.kt
+++ app/src/main/java/com/example/eat_easy/meal_planning.kt
@@ -1,59 +1,111 @@
 package com.example.eat_easy
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.Spinner
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import java.util.Calendar
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [meal_planning.newInstance] factory method to
- * create an instance of this fragment.
- */
 class meal_planning : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var  mealDate:TextView
+    private lateinit var addLayout:Button
+    private lateinit var layoutcontainer :LinearLayout
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meal_planning, container, false)
-    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment meal_planning.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            meal_planning().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val view= inflater.inflate(R.layout.fragment_meal_planning, container, false)
+        addLayout=view.findViewById(R.id.btn_add)
+        layoutcontainer=view.findViewById(R.id.container)
+        mealDate =view.findViewById(R.id.meal_date)
+        addLayout.setOnClickListener{
+            meal_design()
+           addLayout.visibility=View.INVISIBLE
+        }
+        mealDate.setOnClickListener {
+            showDatePickerDialog()
+
+        }
+        return view
     }
+    @SuppressLint("MissingInflatedId")
+    fun meal_design(){
+        val meal_design : View = LayoutInflater.from(requireContext()). inflate(R.layout.meal_design,layoutcontainer,false)
+
+
+        val delete : Button = meal_design.findViewById(R.id.delete)
+        var morining:Spinner=meal_design.findViewById(R.id.Breakfast_spi)
+        val launch:Spinner=meal_design.findViewById(R.id.launch_spi)
+        val dinner:Spinner=meal_design.findViewById(R.id.dinner_spi)
+        val Mitems = listOf("Item 1", "Item 2", "Item 3", "Item 4")
+        val Litems = listOf("Item 1", "Item 2", "Item 3", "Item 4")
+        val Ditems = listOf("Item 1", "Item 2", "Item 3", "Item 4")
+        val Madapter = ArrayAdapter(requireContext(), R.layout.custom_spinner_item, Mitems)
+         morining.adapter=Madapter
+         val Ladapter = ArrayAdapter(requireContext(), R.layout.custom_spinner_item, Litems)
+         Ladapter.setDropDownViewResource(R.layout.custom_spinner_item)
+         launch.adapter = Ladapter
+         val Dadapter = ArrayAdapter(requireContext(), R.layout.custom_spinner_item, Ditems)
+         dinner.adapter = Dadapter
+         delete.setOnClickListener{
+            addLayout.visibility=View.VISIBLE
+            layoutcontainer.removeView(meal_design)
+//            val dialog = AlertDialog.Builder(this)
+//            val dialogView = LayoutInflater.from(this).inflate(R.layout.log_out,null)
+//            dialog.setView(dialogView)
+//
+//            val yes : Button = dialogView.findViewById(R.id.yes)
+//            val no : Button = dialogView.findViewById(R.id.no)
+//
+//            val alertBox = dialog.create()
+//
+//            yes.setOnClickListener{
+//                layoutcontainer.removeView(educationDetailsView)
+//                if(layoutcontainer.childCount==0){
+//                    save.visibility=View.GONE
+//                }
+//            }
+//
+//            no.setOnClickListener{
+//                alertBox.dismiss()
+//            }
+//
+//            alertBox.show()
+
+        }
+
+        layoutcontainer.addView(meal_design)
+    }
+    private fun showDatePickerDialog() {
+        // Get the current date
+        val calendar = Calendar.getInstance()
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+        // Create a DatePickerDialog instance
+        val datePickerDialog = DatePickerDialog(
+            requireContext(), { _, selectedYear, selectedMonth, selectedDay ->
+                val selectedDate = "$selectedDay/${selectedMonth + 1}/$selectedYear"
+                mealDate.text=selectedDate
+            },year,month,day
+        )
+        datePickerDialog.show()
+
+            }
 }
+
+
